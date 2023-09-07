6755b6acfa1156e155393d88824441a1a7048ba5
Prince Jackson <princejackson2687@gmail.com>
2023-09-07 16:34:52 +0100

    Add new files

---
+++ .vscode/java/script.js
@@ -0,0 +1,10 @@
+function showHamburgerMenu(){
+    let hambElem = document.getElementById("mobile-menu");
+    hambElem.style.display = "block";
+}
+
+function closeHamburgerMenu(){
+    let hambElem = document.getElementById("mobile-menu");
+    hambElem.style.display = "none";
+    
+}
